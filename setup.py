ab0ec7009963d7aa62e14ca6a44b0063ba09eef9
Andy Terkik <andy@industrydive.com>
2014-05-25 15:18:56 -0400

    setup and manifest file for setuptools

---
+++ setup.py
@@ -0,0 +1,34 @@
+import os
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-chartbuilder',
+    version='0.1',
+    packages=['django_chartbuilder'],
+    include_package_data=True,
+    license='MIT License',
+    description='A simple Django application for integrating Chartbuilder',
+    long_description=README,
+    url='http://github.com/aterkik/django-chartbuilder',
+    author='Andy Terkik',
+    author_email='aterkik+github@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License', # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        # Replace these appropriately if you are stuck on Python 2.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
